7a438ca171783be93a1b389520c53f262368b07e
Ari Steinfeld <crazybonesx@gmail.com>
2021-09-29 08:33:35 -0400

    YachtSea comp sci project

---
+++ YachtSea.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+
+
+//dR is for diceRoll. I made the name small so that way I don't have to wory about typing it up every time I use it.
+func dR() -> Int{
+    return Int.random(in: 1...6)
+}
+
+func getPnts(roll:[Int]) -> Int{
+    
+    if roll.count != 5{
+        return 0
+    }
+    //check for Yacht Sea
+    
+    
+    
+    
+    
+    return 3
+}
+
+
--- test.playground/Contents.swift
+++ test.playground/Contents.swift
@@ -46,3 +46,4 @@ func getBool(_ value:Bool) -> Bool{
     }
 }
 //pain
+
