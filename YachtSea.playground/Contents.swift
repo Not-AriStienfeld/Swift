83a59450c7a88375323db8746c8ca7c1adda9705
Ari Steinfeld <crazybonesx@gmail.com>
2021-09-30 10:19:17 -0400

    finished YachtSea

--- YachtSea.playground/Contents.swift
+++ YachtSea.playground/Contents.swift
@@ -7,18 +7,97 @@ func dR() -> Int{
     return Int.random(in: 1...6)
 }
 
+//does a turn
+func dRx5() -> [Int]{
+    //rS short for returnStatement
+    var rS = [Int]()
+   
+    for _ in 1...5{
+        rS.append(dR())
+    }
+    return rS
+}
+
+for _ in 1...20{
+    print(getPnts(roll:dRx5()))
+}
+
 func getPnts(roll:[Int]) -> Int{
     
+    //idiot-proofing the code
     if roll.count != 5{
         return 0
     }
-    //check for Yacht Sea
+    //yooo its what we just learned in class!
+    let setRolls = Set(roll)
+    
+    //list of possible values
+    var possibleRoll = [Int]()
+   
+    //parameters are immutable so I had to make a new sorted array
+    var sortedRoll = roll
+    sortedRoll.sort()
+  
+    //a necessary evil
+    var mode = 0
+    var modereps = 0
+    var altModeReps = 0
+    var altMode = 0
+    
+    //yacht Sea option
+    if setRolls.count == 1{
+        return 50
+    }
+    //Small Straight
+    else if setRolls.count == 4{
+        return 30
+    }
+    
+    //Full house
+    else if setRolls.count == 2{
+        return 25
+    }
+    
+    
+    
+    
+    //I couldn't get that other guy's code to work so I made my own
+    for r in 0...sortedRoll.count-2{
+        if sortedRoll[r] == sortedRoll[r+1]{
+            if mode == 0{
+                mode = sortedRoll[r]
+                modereps += 1
+            }
+            if modereps != 0 && sortedRoll[r] == mode{
+                modereps += 1
+            }
+            if sortedRoll[r] != mode{
+                altMode = sortedRoll[r]
+                altModeReps+=1
+            }
+            if (sortedRoll[r] == altMode){
+                altModeReps += 1
+            }
+        }
+    }
+    
+    //finding what gets the most points
+    possibleRoll.append(mode*modereps)
+    possibleRoll.append(altMode*altModeReps)
+    possibleRoll.sort()
+    return possibleRoll[1]
+    
+    //remenants of what once was debugging
+   // print("mode \(mode), modeReps\(modereps) + altmode \(altMode), altmodeReps\(altModeReps)")
     
     
     
     
     
-    return 3
+    
+    
+    
+   
 }
 
 
