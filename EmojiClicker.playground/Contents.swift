56642e2a988d8dcd8e93514befca4c809a246c1f
Ari Steinfeld <crazybonesx@gmail.com>
2021-10-26 11:35:42 -0400

    idiot proofing

--- EmojiClicker.playground/Contents.swift
+++ EmojiClicker.playground/Contents.swift
@@ -4,9 +4,14 @@ class EmojiChooser{
     //instace variables
     var emojiArr = ["😀", "📷", "💽", "🎛", "⛓" ]
     var emojiList : [Int: String] = [1:"😀"]
+    var runable = true
     
     //initialization with a default
     init(_ emojiArr : [String] = ["😀", "📷", "💽", "🎛", "⛓", "⛹️‍♀️", "🥉" , "🥈", "🥇" ]){
+        if emojiArr.count < 5{
+            runable = false
+        }
+        
         self.emojiArr = emojiArr
         
         //sets values to the dictionary
@@ -18,47 +23,54 @@ class EmojiChooser{
     
     //gets the random list of emojis
     func getRandomEmojis() -> [String]{
-       
-        //uses numbers instead of emojis because its generally easier to work with numbers
-        var numEList = [Int.random(in: 0...emojiArr.count-1)]
-        
-        
-        for _ in 1...5{
-            var randomEnough = true
+        if !runable{
+            return ["Improper input"]
+        }
+        else{
+            //uses numbers instead of emojis because its generally easier to work with numbers
+            var numEList = [Int.random(in: 0...emojiArr.count-1)]
             
-            //repeats until it gets a non-repeat emoji. happens 4 time.
-            while randomEnough{
-                let rEmoji = Int.random(in: 0...emojiArr.count-1)
-                if !numEList.contains(rEmoji){
-                    numEList.append(rEmoji)
-                    randomEnough = false
-                    
+            
+            for _ in 1...5{
+                var randomEnough = true
+                
+                //repeats until it gets a non-repeat emoji. happens 4 time.
+                while randomEnough{
+                    let rEmoji = Int.random(in: 0...emojiArr.count-1)
+                    if !numEList.contains(rEmoji){
+                        numEList.append(rEmoji)
+                        randomEnough = false
+                        
+                    }
+                }
+                
+                
+            }
+            //makes one of the random spaces a repeat of the first emoji
+            numEList[Int.random(in: 1...4)] = numEList[0]
+            
+            //makes xcode happy
+            var finalArr : [String] = []
+            
+            //goes from number list to emoji list.
+            for i in 0...4{
+                if let numToEmoji = emojiList[numEList[i]]{
+                    finalArr.append(numToEmoji)
                 }
             }
-            
-            
+            return finalArr
         }
-        //makes one of the random spaces a repeat of the first emoji
-        numEList[Int.random(in: 1...4)] = numEList[0]
-        
-        //makes xcode happy
-        var finalArr : [String] = []
-        
-        //goes from number list to emoji list.
-        for i in 0...4{
-            if let numToEmoji = emojiList[numEList[i]]{
-                finalArr.append(numToEmoji)
-            }
-        }
-        return finalArr
     }
 }
 
 //creates the emojichooser object
-let r = EmojiChooser(["😀", "📷", "💽", "🎛", "⛓", "⚽️", "🛹", "⛸", "🥉" , "🥈", "🥇"])
+let goodCase = EmojiChooser(["😀", "📷", "💽", "🎛", "⛓", "⚽️", "🛹", "⛸", "🥉" , "🥈", "🥇"])
+let badCase = EmojiChooser(["😀"])
 
 //time to see the fruits of my labor
-print(r.getRandomEmojis())
+print(goodCase.getRandomEmojis())
+print(badCase.getRandomEmojis())
+
 
 
 
